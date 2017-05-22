ea1c64731952f3d14d596d3073ddbea14a80a086
Weiming Zhai <wmzhai@gmail.com>
2017-05-22 18:37:42 +0800

    mainwindow class

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const path = require('path')
 const electron = require('electron')
-const TimerTray = require('./app/timer_tray')
 const { app, BrowserWindow, Tray } = electron
+const TimerTray = require('./app/timer_tray')
+const MainWindow = require('./app/main_window')
 
 let mainWindow
 let tray
 
 app.on('ready', () => {
   app.dock.hide()
-  mainWindow = new BrowserWindow({
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false,
-  })
-  mainWindow.loadURL(`file://${__dirname}/src/index.html`)
-  mainWindow.on('blur', () => {
-    mainWindow.hide()
-  })
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`)
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
