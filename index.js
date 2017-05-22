e61f5bd3aac221cda2467d82b2c5118e0536fd35
Weiming Zhai <wmzhai@gmail.com>
2017-05-22 16:30:11 +0800

    toggle window visibility

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const { app, BrowserWindow, Tray } = electron
 
 let mainWindow
+let tray
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -10,10 +11,15 @@ app.on('ready', () => {
     width: 300,
     frame: false,
     resizable: false,
+    show: false,
   })
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-  new Tray(iconPath)
+  tray = new Tray(iconPath)
+  tray.on('click', () => {
+    mainWindow.show()    
+  })
 })
+
