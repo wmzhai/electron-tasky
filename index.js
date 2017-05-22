27994455a491a154c311edbaa2a508756e7f3590
Weiming Zhai <wmzhai@gmail.com>
2017-05-22 17:45:00 +0800

    garbage collection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const TimerTray = require('./app/timer_tray')
 const { app, BrowserWindow, Tray } = electron
 
 let mainWindow
+let tray
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -17,5 +18,5 @@ app.on('ready', () => {
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-  new TimerTray(iconPath, mainWindow)
+  tray = new TimerTray(iconPath, mainWindow)
 })
